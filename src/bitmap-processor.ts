d427801ea5d9f995f18e78e7344e9e7b6b74a9f8
EryegitS <suleymaneryegit@gmail.com>
2022-06-26 16:45:45 +0300

    refactor: comments are added for bitmap processor

--- src/bitmap-processor.ts
+++ src/bitmap-processor.ts
@@ -14,6 +14,11 @@ export class BitmapProcessor {
         north: [-1, 0],
     };
 
+    /**
+     * starts to estimating costs after bitmap is created by reading input file
+     * after all pixels processed, prints the output
+     * @param filePath: path of input file
+     */
     public readInputFile(filePath: string) {
         this.reader = new InputFileReader();
         this.reader.readInputFile(filePath);
@@ -25,37 +30,14 @@ export class BitmapProcessor {
         });
     }
 
-    private isNewPixelInMap(rowIndex: number, columnIndex: number): boolean {
-        return rowIndex >= 0 && columnIndex >= 0 && rowIndex <= this.rowMaxIndex && columnIndex <= this.columnMaxIndex;
-    }
-
-    private getNeighbourPixels(pixel: Pixel): Pixel[] {
-        const neighbours: Pixel[] = [];
-        /** Searching possible neighbour each direction */
-        for (const direction in this.directions) {
-            const newRowIndex = pixel.row + this.directions[direction][0];
-            const newColumnIndex = pixel.column + this.directions[direction][1];
-
-            /** Checkin new pixel value in bitmap */
-            if (this.isNewPixelInMap(newRowIndex, newColumnIndex)) {
-                const neighbour = this.input[newRowIndex][newColumnIndex];
-                /** Checkin new pixel is already hit */
-                if (!neighbour.isHit) {
-                    neighbours.push(neighbour);
-                }
-            }
-        }
-        return neighbours;
-    }
-
+    /**
+     *  White pixels will be origin while estimating neighbour pixels. So first elements of queue are white pixels.
+     *  Firstly finding neighbour pixels and checking their position in bitmap.
+     *  Then checking they are already hit. If not, then adding to queue
+     *  Afterwards set the cost to reach white by help of previous pixel
+     * @private
+     */
     private estimateCosts(): Bitmap<number> {
-        /**
-         *  Basic explanation of the process:
-         *  White pixels will be origin while estimating neighbour pixels. So first elements of queue are white pixels.
-         *  Firstly finding neighbour pixels and checking their position in bitmap.
-         *  Then checking they are already hit. If not, then adding to queue
-         *  Afterwards set the cost to reach white by help of previous pixel
-         */
         const queue: Pixel[] = this.reader.whitePixels.map(white => {
             white.costToWhitePixel = 0;
             white.isHit = true;
@@ -81,14 +63,60 @@ export class BitmapProcessor {
         });
     }
 
+    /**
+     * checks whether new pixel position all into bitmap
+     * @param rowIndex: row index of candidate pixel
+     * @param columnIndex: column index of candidate pixel
+     * @private
+     */
+    private isNewPixelInMap(rowIndex: number, columnIndex: number): boolean {
+        return rowIndex >= 0 && columnIndex >= 0 && rowIndex <= this.rowMaxIndex && columnIndex <= this.columnMaxIndex;
+    }
+
+    /**
+     * get neighbour pixels of the pixel sent as parameter
+     * checking whether is hit already while detecting neighbours
+     * @param pixel
+     * @private
+     */
+    private getNeighbourPixels(pixel: Pixel): Pixel[] {
+        const neighbours: Pixel[] = [];
+
+        /** Searching possible neighbour each direction */
+        for (const direction in this.directions) {
+            const newRowIndex = pixel.row + this.directions[direction][0];
+            const newColumnIndex = pixel.column + this.directions[direction][1];
+
+            /** Checkin new pixel value in bitmap */
+            if (this.isNewPixelInMap(newRowIndex, newColumnIndex)) {
+                const neighbour = this.input[newRowIndex][newColumnIndex];
+                /** Checkin new pixel is already hit */
+                if (!neighbour.isHit) {
+                    neighbours.push(neighbour);
+                }
+            }
+        }
+        return neighbours;
+    }
+
+    /**
+     * gets highest index of row items array
+     */
     get rowMaxIndex(): number {
         return this.reader.getHeightOfBitmap() - 1;
     }
 
+    /**
+     * gets highest index of column items array
+     */
     get columnMaxIndex(): number {
         return this.reader.getWidthOfBitmap() - 1;
     }
 
+    /**
+     * print input of estimation as table
+     * @private
+     */
     private printInput() {
         console.log('Input table:');
         console.table(this.input.map(rows => {
@@ -96,6 +124,10 @@ export class BitmapProcessor {
         }));
     }
 
+    /**
+     * print output of estimation as table
+     * @private
+     */
     private printOutput() {
         console.log('output table:');
         console.table(this.output);
